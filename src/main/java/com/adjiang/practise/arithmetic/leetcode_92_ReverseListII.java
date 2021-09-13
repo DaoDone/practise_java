a15e8be5ff24ed6cfe36ac25322202d377cc3430
DaoDone <l>
2021-09-14 00:48:26 +0800

    day03 learn leetcode92

--- src/main/java/com/adjiang/practise/arithmetic/leetcode_92_ReverseListII.java
+++ src/main/java/com/adjiang/practise/arithmetic/leetcode_92_ReverseListII.java
@@ -14,19 +14,52 @@ import java.util.Stack;
  */
 public class leetcode_92_ReverseListII {
 
+
+    /**
+     * 思路：记录遍历前一个的指针位置和遍历后一个指针位置
+     * 切断链表
+     * @param head
+     * @param left
+     * @param right
+     * @return
+     */
     public static ListNode reverseBetween(ListNode head, int left, int right) {
-        ListNode p = null;
-        for (int i=1;i<left;i++){
-
+        //设置一个虚拟节点
+        ListNode dummyNode = new ListNode(-1);
+        dummyNode.next = head;
+        ListNode prev = dummyNode;
+        //保证 prev 移动到 left -1 的位置
+        for (int i=0;i<left-1;i++){
+            prev = prev.next;
         }
+        //将rightNode移动到right+1的位置
+        ListNode rightNode  = prev;
+        for (int i =0;i <right-left+1;i++){
+            rightNode = rightNode.next;
+        }
+       //切断链表
+        ListNode leftNode =prev.next;
+        ListNode curr=rightNode.next;
+        prev.next=null;
+        rightNode.next=null;
+        //进行链表翻转
+        reverseList1(leftNode);
+        //重新拼接链表
+        prev.next=rightNode;
+        leftNode.next=curr;
+        return  dummyNode.next;
+    }
 
-
-
-
-
-
-
-        return  null;
+    public static ListNode reverseList1(ListNode head) {
+        //初始化两个指针，prev指向前一个节点 curr指向当前节
+        ListNode prev = null, curr = head;
+        while (curr != null) {
+            ListNode tmp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = tmp;
+        }
+        return prev;
     }
 
     // 测试
@@ -34,11 +67,11 @@ public class leetcode_92_ReverseListII {
         ListNode n1 = new ListNode(1);
         ListNode n2 = new ListNode(2);
         ListNode n3 = new ListNode(3);
-        ListNode n4 = new ListNode(3);
+        ListNode n4 = new ListNode(4);
         n1.next = n2;
         n2.next = n3;
         n3.next = n4;
-       ListNode res = reverseBetween(n1,1,2);
+       ListNode res = reverseBetween(n1,2,3);
         while (res != null) {
             System.out.println(res.val);
             res = res.next;
